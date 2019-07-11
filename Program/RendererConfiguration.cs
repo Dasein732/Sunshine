04f967b92b78a151d02bec68d40d29f0a5b4adc8
todor <milostodorovic@protonmail.com>
2019-07-11 21:04:08 +0200

    Create rudimentary renderer config

---
+++ Program/RendererConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    public static class RendererConfiguration
+    {
+        public static bool Antialiasing { get; set; }
+    }
+}
