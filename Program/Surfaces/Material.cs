e9fc22882810aca2a88561d86d63277cf132c905
todor <milostodorovic@protonmail.com>
2019-07-11 22:57:45 +0200

    add material class

---
+++ Program/Surfaces/Material.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Program.Surfaces
+{
+    public abstract class Material
+    {
+        public abstract bool Scatter(in Ray ray, in HitRecord record, Vector3 Attenuation, Ray scattered);
+    }
+}
