2257e3e6c891eba4e48ba508e0c5408305595d57
todor <milostodorovic@protonmail.com>
2019-07-11 15:40:15 +0200

    Create ray class, add basic functionality

---
+++ Program/Ray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Program
+{
+    public sealed class Ray
+    {
+        public Ray(in Vector3 origin, in Vector3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+
+        public Vector3 Origin { get; }
+
+        public Vector3 Direction { get; }
+
+        public Vector3 PointAtParameter(float point) => Origin + point * Direction;
+    }
+}
